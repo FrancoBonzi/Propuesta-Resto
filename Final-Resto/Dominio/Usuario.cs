ab1691904b9320a352c329d0fbf68a585842c3f8
FrancoBonzi <bon.franco12@gmail.com>
2025-01-19 17:38:51 -0300

    commit: class Usuario (Dominio)

---
+++ Final-Resto/Dominio/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+
+        [DisplayName("Contraseña")]
+        public string Contrasenia { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Dni { get; set; }
+    }
+}
