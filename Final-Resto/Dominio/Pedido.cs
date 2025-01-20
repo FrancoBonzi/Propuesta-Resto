bcc0b013e39ff88425667d8d8d4fe61cb3c64895
FrancoBonzi <bon.franco12@gmail.com>
2025-01-20 17:41:12 -0300

    commit: class Pedido (Dominio)

---
+++ Final-Resto/Dominio/Pedido.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Pedido
+    {
+        public int IdPedido { get; set; }
+
+        public DateTime FechaPedido { get; set; }
+
+        public int IdMesa { get; set; }
+
+        public int IdArticulo { get; set; }
+
+        public int IdMozo { get; set; }
+    }
+}
